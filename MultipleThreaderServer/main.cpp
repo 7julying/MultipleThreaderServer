77fb4b25c045f4b48a480519bbab1bfe524026e8
ninaccl <30490041+7julying@users.noreply.github.com>
2018-03-28 10:37:49 +0800

    go to point
    实现功能：
    定点运动(通过二维坐标估计时间)
    主车接受指令后执行并反馈，服务器转发其反馈控制从车运动
    解决了上一版监听线程错误10035及10004——错误原因，主函数没有加while循环语句，导致线程随着主函数关闭而关闭，没有一直在后台执行。

--- MultipleThreaderServer/main.cpp
+++ MultipleThreaderServer/main.cpp
@@ -9,9 +9,12 @@
 #include <windows.h>
 #include <conio.h>
 #include <time.h>
+#include <math.h>
+
 // Need to link with Ws2_32.lib
 using namespace std;
 #pragma comment(lib, "ws2_32.lib")			//动态库函数
+
 
 /**
 * 全局变量
@@ -19,7 +22,7 @@ using namespace std;
 /**
 *函数申明
 */
-BOOL initSever(void);                       //初始化
+BOOL initServer(void);                       //初始化
 void initMember(void);
 bool initSocket(void);						//初始化非阻塞套接字
 void exitServer(void);						//释放资源
@@ -28,6 +31,10 @@ void showServerStartMsg(BOOL bSuc);         //显示错误信息
 void showServerExitMsg(void);               //显示退出消息
 BOOL createAcceptThread(void);      //开启监控函数
 DWORD __stdcall acceptThread(void* pParam); //开启客户端请求线程
+DWORD WINAPI timerThreadProc(__in LPVOID lpParameter);//开启计时器线程
+void readyState(void);						//小车处于准备状态
+void planState(void);						//小车处于计算状态
+void moveState(void);						//小车处于运动状态
 /*** 全局变量
 */
 char	dataBuf[2];				//写缓冲区
@@ -35,13 +42,47 @@ BOOL	bConning;							//与客户端的连接状态
 BOOL    bSend;                              //发送标记位
 BOOL    clientConn;                         //连接客户端标记
 SOCKET	listenSocket;							//服务器监听套接字
-HANDLE	hAcceptThread;						//数据处理线程句柄
+HANDLE	hAcceptThread;						//接受客户端连接线程句柄
+HANDLE  hHelpThread;
 HANDLE	hCleanThread;						//数据接收线程
+int timerFlag;									//计时器标志位-计时器下一状态
+int timeCount;									//运动时间
+int carState;									//1代表READY,2代表PLAN,3代表MOVE
+int carDirection;								//1代表UP，2代表DOWN,3代表LEFT，4代表RIGHT
+int carMoveState;								//1代表STRAIGHT，2代表WAIT,3代表LEFT，4代表RIGHT
+int masterSocketFlag;						//主车发送标志位
+int bufCount;									//接受主车返回数据个数
+char masterSocketStr[5];						//主车
+int connectNum;									//连接个数
+char slaveSocketFlag[5];						//从车发送标志位
+char slaveSocketStr[5];						//从车
+int obstacleFlag;								//障碍标志位
+int xDistance;
+int yDistance;
+int xcopy;
+int ycopy;
+int test = 0;
 
-											/**
-											* 初始化
-											*/
-BOOL initSever(void)
+#define READY 1	//等待输入坐标
+#define PLAN 2	//计算路径
+#define MOVE 3	//小车运动
+#define UP 1	//车头朝前
+#define DOWN 2	//车头朝后
+#define LEFT 3	//车头朝左或小车左转
+#define RIGHT 4	//车头朝右或小车右转
+#define STRAIGHT 1	//小车前进
+#define WAIT 2	//维持当前状态等待计时器计时结束
+#define ORDER_STRAIGHT 0x01
+#define ORDER_STOP 0x02
+
+
+#define ORDER_LEFT 0x03
+#define ORDER_RIGHT 0x04
+#define TURN_TIME 1000//转90度角所用时间
+#define SERVER_SETUP_FALL 1//启动服务器失败
+ 
+//初始化
+BOOL initServer(void)
 {
 	//初始化全局变量
 	initMember();
@@ -53,23 +94,34 @@ BOOL initSever(void)
 	return TRUE;
 }
 
-/**
-* 初始化全局变量
-*/
+//初始化全局变量
 void initMember(void)
 {
-	memset(dataBuf, 0, 2);
 	bSend = FALSE;
 	clientConn = FALSE;
 	bConning = FALSE;									    //服务器为没有运行状态
 	hAcceptThread = NULL;									//设置为NULL
 	hCleanThread = NULL;
 	listenSocket = INVALID_SOCKET;								//设置为无效的套接字
+	timerFlag = 0;
+	timeCount = 0;
+	carState = 1;
+	carDirection = 1;
+	carMoveState = 2;
+	masterSocketFlag = 0;
+	memset(masterSocketStr, 0, 5);
+	connectNum = 0;
+	memset(slaveSocketFlag, 0, 5);
+	memset(slaveSocketStr, 0, 5);
+	bufCount = 0;
+	obstacleFlag = 0;
+	xDistance = -1;
+	yDistance = -1;
+	xcopy = -1;
+	ycopy = -1;
 }
 
-/**
-*  初始化SOCKET
-*/
+//初始化SOCKET
 bool initSocket(void)
 {
 	//返回值
@@ -88,13 +140,6 @@ bool initSocket(void)
 		return 1;
 	}
 
-	//设置套接字非阻塞模式
-	
-	/*unsigned long ul = 1;
-	reVal = ioctlsocket(listenSocket, FIONBIO, (unsigned long*)&ul);
-	if (SOCKET_ERROR == reVal)
-		return FALSE;*/
-
 	//绑定套接字
 	sockaddr_in serAddr;
 	serAddr.sin_family = AF_INET;
@@ -112,9 +157,7 @@ bool initSocket(void)
 	return TRUE;
 }
 
-/**
-*  启动服务
-*/
+//启动服务
 bool startService(void)
 {
 	BOOL reVal = TRUE;	//返回值
@@ -128,9 +171,8 @@ bool startService(void)
 	return reVal;
 }
 
-/**
-* 接受客户端连接线程
-*/
+
+//接受客户端连接线程
 BOOL createAcceptThread(void)
 {
 	bConning = TRUE;//设置服务器为运行状态
@@ -160,11 +202,61 @@ DWORD WINAPI carMasterThreadProc(
 	SOCKET AcceptSocket = (SOCKET)lpParameter;
 	//设置套接字非阻塞模式
 	nRet = ioctlsocket(AcceptSocket, FIONBIO, (unsigned long*)&ul);
-	printf("master car\n");
+	//printf("master car\n");
 	if (nRet == SOCKET_ERROR)
 	{
 		//设置套接字非阻塞模式，失败处理
 	}
+
+	printf("Master Car Connect\n");
+
+	while (1)
+	{
+		int c;
+		if (carState != READY)
+		{
+			if (_kbhit())//检测键盘有无按键，有按键_kbhit()返回一个非零值
+			{
+				//按键1代表前进，按键2代表停止
+				std::cin >> c;
+				if (c != 0)
+				{
+					if (c == 1)
+					{
+						timerFlag = 0;
+					}
+					c = 0;//清空按键状态信息，避免误判断
+				}
+			}
+		}
+		char buf[51] = { 0 };
+		memset(buf, 0, 51);//每循环一次就清空接收数据
+		bufCount = recv(AcceptSocket, buf, 50, 0);
+		if (SOCKET_ERROR == bufCount)
+		{
+			int err = WSAGetLastError();
+			if (WSAETIMEDOUT == err || WSAENETDOWN == err)
+			{
+				break;
+			}
+		}
+		/*
+		if(遇到障碍)
+		{
+		timerFlag=0;
+		}
+		*/
+		if (bufCount > 0)
+		{
+			memset(slaveSocketFlag, 1, 6);//初始化sendflag数组，每一位分别代表一个客户端的发送状态，没有发送为1
+			memcpy(slaveSocketStr, buf, sizeof slaveSocketStr);
+		}
+		if (masterSocketFlag == 1)
+		{
+			send(AcceptSocket, masterSocketStr, 5, 0);
+			masterSocketFlag = 0;
+		}
+	}	
 	//结束连接
 	closesocket(AcceptSocket);
 	return 0;
@@ -179,32 +271,43 @@ DWORD WINAPI carSlaveThreadProc(
 	SOCKET AcceptSocket = (SOCKET)lpParameter;
 	//设置套接字非阻塞模式
 	nRet = ioctlsocket(AcceptSocket, FIONBIO, (unsigned long*)&ul);
-	printf("slave car\n");
+	//printf("slave car\n");
 	if (nRet == SOCKET_ERROR)
 	{
 		//设置套接字非阻塞模式，失败处理
 	}
+	int num = connectNum;
+	printf("Slave Car %d Connect\n", num);
+	
+	while (1)
+	{
+		if (bufCount > 0)
+		{
+			if (slaveSocketFlag[num])
+			{
+				send(AcceptSocket, slaveSocketStr, 5, 0);
+				slaveSocketFlag[num] = 0;//发送数据后将数组中的第number位置0，代表已发送
+			}
+		}
+	}
+
 	//结束连接
 	closesocket(AcceptSocket);
 	return 0;
 }
 
-/**
-* 接受客户端连接
-*/
+//接受客户端连接
 DWORD __stdcall acceptThread(void* pParam)
 {
 								                        //接受客户端连接的套接字
 	sockaddr_in addrClient;						                        //客户端SOCKET地址
 	int	lenClient = sizeof(sockaddr_in);				        	//地址长度
-	int connectNum = 0;//连接个数
 	while (bConning)						                                //服务器的状态
 	{			
 		SOCKET  acceptSocket = accept(listenSocket, (sockaddr*)&addrClient, &lenClient);	//接受客户请求
 		if (INVALID_SOCKET == acceptSocket)
 		{
 			int x = GetLastError();
-			Sleep(1);
 			continue;
 		}
 		else//接受客户端的请求
@@ -236,15 +339,12 @@ DWORD __stdcall acceptThread(void* pParam)
 				}
 				CloseHandle(hThread);
 			}
-			connectNum++;
 		}
 	}
 	return 0;//线程退出
 }
 
-/**
-*  释放资源
-*/
+//释放资源
 void  exitServer(void)
 {
 	closesocket(listenSocket);					//关闭SOCKET
@@ -252,9 +352,7 @@ void  exitServer(void)
 	WSACleanup();							//卸载Windows Sockets DLL
 }
 
-/**
-* 显示启动服务器成功与失败消息
-*/
+//显示启动服务器成功与失败消息
 void  showServerStartMsg(BOOL bSuc)
 {
 	if (bSuc)
@@ -271,9 +369,7 @@ void  showServerStartMsg(BOOL bSuc)
 
 }
 
-/**
-* 显示服务器退出消息
-*/
+//显示服务器退出消息
 void  showServerExitMsg(void)
 {
 
@@ -282,10 +378,50 @@ void  showServerExitMsg(void)
 	cout << "**********************" << endl;
 }
 
+DWORD WINAPI timerThreadProc(
+__in LPVOID lpParameter
+)
+{
+	clock_t startTime;
+	clock_t endTime;
+	int timerFlaglast = 0;
+	while (1)
+	{
+		if (timerFlag == 1)
+		{
+			if (timerFlag==timerFlaglast)
+			{
+				endTime = clock();
+				if ((endTime-startTime)>timeCount)
+				{
+					cout << "结束计时";
+					timerFlag = 0;
+					timeCount = 0;
+				}
+			}
+			else
+			{
+				cout << "开始计时";
+				startTime = clock();
+			}
+		}
+		else
+		{
+			if (timerFlag != timerFlaglast)
+			{
+				endTime = clock();
+				timeCount = timeCount - (endTime - startTime);
+			}
+		}
+			timerFlaglast = timerFlag;
+		Sleep(1);
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	//初始化服务器
-	if (!initSever())
+	if (!initServer())
 	{
 		exitServer();
 		return 1;
@@ -299,10 +435,223 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
+	//启动定时器线程
+	DWORD thread;
+	HANDLE hThread = CreateThread(NULL, 0, timerThreadProc, NULL, 0, &thread);
+	if (hThread == NULL)
+	{
+		printf("Thread Creat Failed!\n");
+	}
+	else
+	{
+		printf("Thread Creat Successful!\n");
+	}
 	//处理数据
+	char c;
+	cout << "键盘键入s开始运动:" << endl;
+	std::cin >> c;
+	if (c == 's' || c == 'S')
+	{
+		while (1)
+		{
+				switch (carState)
+				{
+				case READY:		
+					readyState();
+					break;
+				case PLAN:
+					planState();
+					break;
+				case MOVE:
+					moveState();
+					break;
+				default:
+					break;
+				}	
+		}
+	}
 
 	//退出主线程，清理资源
 	exitServer();
 
 	return 0;
+}
+
+void readyState()
+{
+			cout << "输入目的点坐标:" << endl;
+			int xcurrent = 4;
+			int ycurrent = 5;
+			test = 5;
+			cout << "输入x坐标" << endl;
+			cin >> xcurrent;
+			cout << "输入y坐标" << endl;
+			cin >> ycurrent;
+			xDistance = xcurrent * 1000;
+			yDistance = ycurrent * 1000;
+			xcopy = xDistance;
+			ycopy = yDistance;
+			carState = PLAN;
+}
+
+void planState()
+{
+	if (xDistance == 0 && yDistance == 0)
+	{
+		printf("运动到指定位置\n");
+		carState = READY;
+	}
+	else
+	{
+		switch (carDirection)
+		{
+		case UP:
+			if(yDistance<=0)
+			{
+				if (xDistance > 0)
+				{
+					carMoveState = RIGHT;
+					carDirection = RIGHT;
+				}
+				else
+				{
+					carMoveState = LEFT;
+					carDirection = LEFT;
+				}
+			}
+			else
+			{
+				carMoveState = STRAIGHT;
+				yDistance = 0;
+			}
+			break;
+		case DOWN:
+			if (yDistance >=0)
+			{
+				if (xDistance > 0)
+				{
+					carMoveState = LEFT;
+					carDirection = RIGHT;
+				}
+				else
+				{
+					carMoveState = RIGHT;
+					carDirection = LEFT;
+				}
+			}
+			else
+			{
+				carMoveState = STRAIGHT;
+				yDistance = 0;
+			}
+			break;
+		case LEFT:
+			if (xDistance < 0)
+			{
+				carMoveState = STRAIGHT;
+				xDistance = 0;
+			}
+			else
+			{
+				if (yDistance < 0)
+				{
+					carMoveState = LEFT;
+					carDirection = DOWN;
+				}
+				else
+				{
+					carMoveState = RIGHT;
+					carDirection = UP;
+				}
+			}
+			break;
+		case RIGHT:
+			if (xDistance > 0)
+			{
+				carMoveState = STRAIGHT;
+				xDistance = 0;
+			}
+			else
+			{
+				if (yDistance < 0)
+				{
+					carMoveState = RIGHT;
+					carDirection = DOWN;
+				}
+				else
+				{
+					carMoveState = LEFT;
+					carDirection = UP;
+				}
+			}
+			break;
+		default:
+			break;
+		}
+		carState = MOVE;
+	}
+}
+
+void moveState()
+{
+	switch (carMoveState)
+	{
+	case STRAIGHT:
+		masterSocketStr[1] = ORDER_STRAIGHT;
+		masterSocketFlag = 1;
+		if (carDirection == UP || carDirection == DOWN)
+		{
+			timeCount = abs(ycopy);
+		}
+		else
+		{
+			timeCount = abs(xcopy);
+		}
+		timerFlag = 1;
+		carMoveState = WAIT;
+		break;
+	case LEFT:
+		masterSocketStr[1] = ORDER_LEFT;
+		masterSocketFlag = 1;
+		timeCount = TURN_TIME;
+		timerFlag = 1;
+		carMoveState = WAIT;
+		break;
+	case RIGHT:
+		masterSocketStr[1] = ORDER_RIGHT;
+		masterSocketFlag = 1;
+		timeCount = TURN_TIME;
+		timerFlag = 1;
+		carMoveState = WAIT;
+		break;
+	case WAIT:
+		if (timerFlag == 0)
+		{
+			masterSocketStr[1] = ORDER_STOP;
+			masterSocketFlag = 1;
+			carState = PLAN;
+			Sleep(1000);
+			if (timeCount != 0)
+			{
+				switch (carDirection)
+				{
+				case UP:
+					yDistance = timeCount;
+					break;
+				case DOWN:
+					yDistance = -timeCount;
+					break;
+				case LEFT:
+					xDistance = -timeCount;
+				case RIGHT:
+					xDistance = timeCount;
+				default:
+					break;
+				}
+			}
+		}
+		break;
+	default:
+		break;
+	}
 }
